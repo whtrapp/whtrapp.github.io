27b28b3defee6ef1e244591e92fe37e2c7ebab3a
AalokeCode <cuba2419@gmail.com>
2024-10-21 14:33:44 +0530

    Updated button CSS, Removed Font Awesome, Removed minfied files.

--- js/main.js
+++ js/main.js
@@ -171,7 +171,7 @@ document.addEventListener("DOMContentLoaded", function () {
           dayOutput.innerHTML = `${dayOfTheWeek(d, m, y)}`;
           theDate = `${y}-${m}-${d}`;
   
-          nameOutput.innerHTML = `<i class="fas fa-location-dot"></i> ${data.location.name}, ${data.location.country}`;
+          nameOutput.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256"><path d="M128,16a88.1,88.1,0,0,0-88,88c0,75.3,80,132.17,83.41,134.55a8,8,0,0,0,9.18,0C136,236.17,216,179.3,216,104A88.1,88.1,0,0,0,128,16Zm0,56a32,32,0,1,1-32,32A32,32,0,0,1,128,72Z"></path></svg> ${data.location.name}, ${data.location.country}`;
           const iconID = data.current.condition.icon.substr(
             "//cdn.weatherapi.com/weather/64x64/".length
           );
