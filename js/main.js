aca4481a4f430b5f4b8a95707fdfdfc59037510a
Vivek Anand Singh <17vivekanandsingh@gmail.com>
2024-10-26 05:32:25 +0530

    Switched the typing effect to place in bottom

--- js/main.js
+++ js/main.js
@@ -66,7 +66,7 @@ document.addEventListener('DOMContentLoaded', function () {
             dateOutput.innerHTML = `${getMonth(d, m, y)} ${d}, ${y}`;
             timeOutput.innerHTML = time;
             dayOutput.innerHTML = dayOfTheWeek(d, m, y);
-            nameOutput.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256"><path d="M128,16a88.1,88.1,0,0,0-88,88c0,75.3,80,132.17,83.41,134.55a8,8,0,0,0,9.18,0C136,236.17,216,179.3,216,104A88.1,88.1,0,0,0,128,16Zm0,56a32,32,0,1,1-32,32A32,32,0,0,1,128,72Z"></path></svg> ${weatherData.location.name}, ${weatherData.location.country}`;
+            // nameOutput.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256"><path d="M128,16a88.1,88.1,0,0,0-88,88c0,75.3,80,132.17,83.41,134.55a8,8,0,0,0,9.18,0C136,236.17,216,179.3,216,104A88.1,88.1,0,0,0,128,16Zm0,56a32,32,0,1,1-32,32A32,32,0,0,1,128,72Z"></path></svg> ${weatherData.location.name}, ${weatherData.location.country}`;
             place = weatherData.location.name + ", " + weatherData.location.country;
             typingEffect();
 
