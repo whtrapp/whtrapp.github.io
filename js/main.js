f57adb0f49a14a6b819a8acf0a401050ad1b9c42
satya <satyanandgupta845@gmail.com>
2024-10-27 22:12:34 +0530

    added some features like in search bar added curser pointer and time is to visible in seconds and also added  some animations

--- js/main.js
+++ js/main.js
@@ -51,63 +51,61 @@ document.addEventListener('DOMContentLoaded', function () {
     'Sydney',
     'Dubai',
   ];
-
-  async function updateWeatherDisplay(city = cityInput) {
+async function updateWeatherDisplay(city = cityInput) {
     try {
-      const weatherData = await fetchWeatherData(city);
-      const date = weatherData.location.localtime;
-      const y = parseInt(date.substr(0, 4));
-      const m = parseInt(date.substr(5, 2));
-      const d = parseInt(date.substr(8, 2));
-      const time = date.substr(11);
-      const theDate = `${y}-${m}-${d}`;
+        const weatherData = await fetchWeatherData(city);
+        
+        // Extract date and time from the weather data
+        const date = weatherData.location.localtime;
+        const y = parseInt(date.substr(0, 4));
+        const m = parseInt(date.substr(5, 2));
+        const d = parseInt(date.substr(8, 2));
+        const time = date.substr(11); // This extracts "HH:MM:SS"
+        const theDate = `${y}-${m}-${d}`;
 
-            // Update UI elements
-            temp.innerHTML = `${weatherData.current.temp_c}&#176;C / ${weatherData.current.temp_f}&#176;F`;
-            dateOutput.innerHTML = `${getMonth(d, m, y)} ${d}, ${y}`;
-            timeOutput.innerHTML = time;
-            dayOutput.innerHTML = dayOfTheWeek(d, m, y);
-            // nameOutput.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#fff" viewBox="0 0 256 256"><path d="M128,16a88.1,88.1,0,0,0-88,88c0,75.3,80,132.17,83.41,134.55a8,8,0,0,0,9.18,0C136,236.17,216,179.3,216,104A88.1,88.1,0,0,0,128,16Zm0,56a32,32,0,1,1-32,32A32,32,0,0,1,128,72Z"></path></svg> ${weatherData.location.name}, ${weatherData.location.country}`;
-            place = weatherData.location.name + ", " + weatherData.location.country;
-            typingEffect();
+        // Update UI elements
+        temp.innerHTML = `${weatherData.current.temp_c}&#176;C / ${weatherData.current.temp_f}&#176;F`;
+        dateOutput.innerHTML = `${getMonth(d, m, y)} ${d}, ${y}`;
+        timeOutput.innerHTML = time;
+        dayOutput.innerHTML = dayOfTheWeek(d, m, y);
+        place = weatherData.location.name + ", " + weatherData.location.country;
+        typingEffect();
 
-            const iconID = weatherData.current.condition.icon.substr(
-                '//cdn.weatherapi.com/weather/64x64/'.length
-            );
-            icon.src = './img/icons/' + iconID;
+        // Set the icon from Weather API
+        const iconID = weatherData.current.condition.icon.substr(
+            '//cdn.weatherapi.com/weather/64x64/'.length
+        );
+        icon.src = './img/icons/' + iconID;
 
-            const [astronomyData, forecastData] = await Promise.all([
-                fetchAstronomyData(city, theDate),
-                fetchForecastData(city),
-            ]);
+        // Fetch and display additional astronomy and forecast data
+        const [astronomyData, forecastData] = await Promise.all([
+            fetchAstronomyData(city, theDate),
+            fetchForecastData(city),
+        ]);
 
-            sunriseOutput.innerHTML =
-                astronomyData.astronomy.astro.sunrise;
-            sunsetOutput.innerHTML =
-                astronomyData.astronomy.astro.sunset;
-            tomorrowOutput.innerHTML = `Tomorrow's Temperature: ${forecastData.current.temp_c}&#176;C / ${forecastData.current.temp_f}&#176;F`;
+        sunriseOutput.innerHTML = astronomyData.astronomy.astro.sunrise;
+        sunsetOutput.innerHTML = astronomyData.astronomy.astro.sunset;
+        tomorrowOutput.innerHTML = `Tomorrow's Temperature: ${forecastData.current.temp_c}&#176;C / ${forecastData.current.temp_f}&#176;F`;
 
-            cloudOutput.innerHTML = weatherData.current.cloud + '%';
-            humidityOutput.innerHTML =
-                weatherData.current.humidity + '%';
-            windOutput.innerHTML =
-                weatherData.current.wind_kph + 'km/h';
-            PrecipitationOutput.innerHTML =
-                weatherData.current.precip_mm + 'mm';
+        cloudOutput.innerHTML = weatherData.current.cloud + '%';
+        humidityOutput.innerHTML = weatherData.current.humidity + '%';
+        windOutput.innerHTML = weatherData.current.wind_kph + 'km/h';
+        PrecipitationOutput.innerHTML = weatherData.current.precip_mm + 'mm';
 
-            const timeOfDay = weatherData.current.is_day
-                ? 'day'
-                : 'night';
-            updateUI(app, weatherData, timeOfDay, exploreBtn);
+        // Determine time of day and update the UI
+        const timeOfDay = weatherData.current.is_day ? 'day' : 'night';
+        updateUI(app, weatherData, timeOfDay, exploreBtn);
 
-            saveToLocalStorage(weatherData.location.name);
-            displayLastVisited();
-            app.style.opacity = '1';
-        } catch (error) {
-            console.error('Error updating weather display:', error);
-            app.style.opacity = '1';
-        }
+        // Save the last visited city to local storage
+        saveToLocalStorage(weatherData.location.name);
+        displayLastVisited();
+        app.style.opacity = '1';
+    } catch (error) {
+        console.error('Error updating weather display:', error);
+        app.style.opacity = '1';
     }
+}
+
 
     function displayLastVisited() {
         const storedData = localStorage.getItem('lastVisited');
